60dda677a0d64e16aafb72e5f264a4adb24696fc
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2020-10-23 00:12:48 -0500

    Update funct.py

--- Python/test/funct.py
+++ Python/test/funct.py
@@ -1,14 +1,22 @@
 largest = None
 smallest = None
+nums=[]
 while True:
     
-    num=int(input("numero: "))
-    val= input("Enter a number: ")
 
-    if val == "done" :
+    try:
+        num=int(input("numero: "))
+        val= input("Enter a number: ")
+        if not(num==int):
+            nums.append(num)
+            nums.sort()
+    except ValueError:
+        print("Invalid Input")
+        continue
+    if val == "done" : 
+        smallest=nums[0]
+        largest= nums[3]
         break
-    print(num)
 
-        
-
-print("Maximum", largest)
+print("Maximum is", largest)
+print("Minimum is ",smallest)
