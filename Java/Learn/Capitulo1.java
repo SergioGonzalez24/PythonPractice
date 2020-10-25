fa28d876386003f7b5b8cd9326ee4c9ce1124ab7
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2020-10-25 02:38:55 -0600

    update

--- Java/Learn/Capitulo1.java
+++
@@ -1,6 +0,0 @@
-public class Capitulo1 {
-    public static void main(String[] args) {
-        //primer programa en java
-        System.out.println("Hello, World");
-    }
-}
--- Java/Learn/suma_variables.java
+++
@@ -1,9 +0,0 @@
-public class suma_variables {
-    public static void main(String[] args) {
-        var x=1;
-        var y=5;
-
-        System.out.println(x+y);
-    }
-    
-}
